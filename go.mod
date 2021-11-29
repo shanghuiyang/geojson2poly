1ce3deb2a48f52cfa1974365ef16b8834bf6cc5b
Shanghui Yang <shanghui.yang@grabtaxi.com>
2021-11-30 00:20:13 +0800

    use go.geojson package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shanghuiyang/geojson2poly
 
 go 1.15
+
+require github.com/paulmach/go.geojson v1.4.0
--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
-	"github.com/shanghuiyang/geojson2poly/geojson"
+	geojson "github.com/paulmach/go.geojson"
 )
 
 func main() {
 	if len(os.Args) != 5 {
-		fmt.Println("error: invalid args")
+		fmt.Println("ERROR! invalid args")
 		fmt.Println("usage: geojson2poly -i infile.json -o outfile.poly")
 		os.Exit(1)
 	}
@@ -27,21 +28,61 @@ func main() {
 	fmt.Printf("input: %v\n", input)
 	fmt.Printf("output: %v\n", output)
 
-	g := geojson.NewGeojson()
-	err := g.Load(input)
+	rawJSON, err := ioutil.ReadFile(input)
 	if err != nil {
-		fmt.Printf("error: fail to load geojson from: %v\n", input)
-		fmt.Printf("err msg: %v\n", err)
+		fmt.Printf("ERROR! %v\n", err)
+		os.Exit(1)
+
+	}
+
+	f, err := geojson.UnmarshalFeature(rawJSON)
+	if err != nil {
+		fmt.Printf("ERROR! %v\n", err)
 		os.Exit(1)
 	}
 
-	err = g.ToPoly(output)
-	if err != nil {
-		fmt.Printf("error: fail to save poly file: %v\n", output)
-		fmt.Printf("err msg: %v\n", err)
+	if f.Type != "Feature" {
+		fmt.Println("ERROR! unsupported geojson")
 		os.Exit(1)
 	}
 
-	fmt.Println("done in success")
+	result := "no_name"
+	if name, err := f.PropertyString("name"); err == nil {
+		result = name
+	}
+	result += "\n"
+
+	switch f.Geometry.Type {
+	case geojson.GeometryPolygon:
+		result += "1\n"
+		result += toPoly(f.Geometry.Polygon)
+		result += "END\n"
+	case geojson.GeometryMultiPolygon:
+		for i, p := range f.Geometry.MultiPolygon {
+			result += fmt.Sprintf("%v\n", i+1)
+			result += toPoly(p)
+			result += "END\n"
+		}
+	}
+	result += "END\n"
+	if err := ioutil.WriteFile(output, []byte(result), 0644); err != nil {
+		fmt.Printf("ERROR! %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("success")
 	os.Exit(0)
+}
+
+func toPoly(polygon [][][]float64) string {
+	if len(polygon) == 0 {
+		return ""
+	}
+
+	res := ""
+	for _, line := range polygon {
+		for _, pt := range line {
+			res += fmt.Sprintf("\t%v %v\n", pt[0], pt[1])
+		}
+	}
+	return res
 }
